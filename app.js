a3dddc0ba4e4c7bfadf8431200caa0c0f9713918
sunil yadav <bca24276117@gmail.com>
2026-02-25 15:14:18 +0530

    all curd operation is done

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const listing = require('./models/listing.js');
+const path = require('path');
+const methodOverride = require('method-override')
+
+ 
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
 
 main().then(()=>{
     console.log('connection is done!');
@@ -17,20 +27,71 @@ app.get('/',(req,res)=>{
     res.send('i am root');
 })
 
-app.get('/testing', (req,res)=>{
-    let sample =  new listing({
-        title:"my new villa",
-        description:"by the beach",
-        price:1000,
-        location:"jalandhar",
-        country:"india"
-    })
-    sample.save().then((res)=>{
-        console.log(res);
-    })
-    
-    res.send("done");
+
+//index route
+app.get('/listing',async (req,res)=>{
+    let alllistings = await listing.find();
+    res.render('index.ejs',{ alllistings });
+
 })
+
+app.get('/listing/new',(req,res)=>{
+    
+    res.render('create.ejs');
+})
+app.post('/listing',async(req,res)=>{
+    let{title,description,image,price,location,country} = req.body;
+    let insert = await listing.insertOne({title,description,image,price,location,country});
+    
+    res.redirect('/listing');
+
+})
+
+app.get('/listing/:id/edit',async(req,res)=>{
+    let { id } = req.params;
+    let item = await listing.findById(id);
+    res.render('edit.ejs',{item});
+})
+
+app.put("/listing/:id",async(req,res)=>{
+    let { id } = req.params;
+    let{title,description,image,price,location,country} = req.body;
+    let update = await listing.findByIdAndUpdate(id,{title,description,image,price,location,country});
+    res.redirect(`/listing/${id}`);
+
+
+})
+app.delete('/listing/:id',async(req,res)=>{
+    let {id} = req.params;
+    let d = await listing.findByIdAndDelete(id);
+    res.redirect('/listing');
+
+})
+
+
+app.get('/listing/:id',async(req,res)=>{
+    let { id } = req.params;
+    let item = await listing.findById(id);
+    res.render('show.ejs',{item});
+})
+
+
+
+
+// app.get('/testing', (req,res)=>{
+//     let sample =  new listing({
+//         title:"my new villa",
+//         description:"by the beach",
+//         price:1000,
+//         location:"jalandhar",
+//         country:"india"
+//     })
+//     sample.save().then((res)=>{
+//         console.log(res);
+//     })
+    
+//     res.send("done");
+// })
 
 app.listen(8080,()=>{
 
