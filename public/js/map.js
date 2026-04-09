fb18793d3b1f714134291189ebca74ad95b321ff
sunil yadav <bca24276117@gmail.com>
2026-04-09 11:43:39 +0530

    problem solve

--- public/js/map.js
+++ public/js/map.js
@@ -1,22 +1,23 @@
 
 
+  document.addEventListener("DOMContentLoaded", () => {
+  const coords = window.placesData; 
 
-const coords = window.placesData;
+  if (!coords) {
+    console.error("No coordinates found");
+    return;
+  }
 
-const lat = coords.lat;
-const lon = coords.lon;
+  const lat = coords.lat;
+  const lon = coords.lon;
 
-// map create
-const map = L.map('map').setView([lat, lon], 20);
+  const map = L.map('map').setView([lat, lon], 15);
 
-// tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 9
-}).addTo(map);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+  }).addTo(map);
 
-// marker
-L.marker([lat, lon]).addTo(map)
-  .bindPopup("welcome to WanderLust")
-  .openPopup();
-
-
+  L.marker([lat, lon]).addTo(map)
+    .bindPopup("welcome to WanderLust")
+    .openPopup();
+});
